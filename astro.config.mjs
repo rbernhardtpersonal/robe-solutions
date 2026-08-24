d0ebcb9c5f937a79e842ca992fe92389a1c4cc14
rbernhardtpersonal <rbernhardtpersonal@users.noreply.github.com>
2026-08-24 15:04:27 +0100

    Refine themes and navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://rbernhardtpersonal.github.io',
-	base: '/robe-solutions',
+	base: process.env.GITHUB_ACTIONS ? '/robe-solutions' : undefined,
 });
